54d77720e56f41a1f23385ee1e1be1f5c71ca698
JasonMcGee0715 <jdm0715@gmail.com>
2020-07-07 17:29:54 -0500

    5 tests passed / 4 more needed

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -9,12 +9,24 @@ return num % 2 === 0 ? true : false;
 
 const sum = (arr) => {
   // write code for numbers.sum
+  var sum = arr.reduce(function(a,b) {
+    return a + b;
+  })
+  return sum;
+} 
 
-}
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
-
+  var total = arr.reduce(function(a,b) {
+    return a + b;
+  })
+  if (total == sum) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 module.exports = {
